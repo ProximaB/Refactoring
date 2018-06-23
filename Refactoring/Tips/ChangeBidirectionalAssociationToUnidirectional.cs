e3e560e46bbd3edf5f4fadcf800ff1f90c88670f
Piotr Czech <piterczech@live.com>
2018-06-23 21:59:49 +0200

    commit

---
+++ Refactoring/Tips/ChangeBidirectionalAssociationToUnidirectional.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+
+namespace Refactoring.Tips
+{
+	public class ChangeBidirectionalAssociationToUnidirectional
+	{
+		public class OrderBefore
+		{
+			public CustomerBefore Customer { get; set; }
+
+			public void SetCustomer(CustomerBefore arg)
+			{
+				Customer?.FriendOrders().Remove(this);
+				Customer = arg;
+				Customer?.FriendOrders().Add(this);
+			}
+
+			public double GetDiscountedPrice() => GetGrossPrice() * (1 - Customer.GetDiscount());
+
+			public double GetGrossPrice() => 0;
+		}
+
+		public class CustomerBefore
+		{
+			private readonly HashSet<OrderBefore> _orders = new HashSet<OrderBefore>();
+
+			private void AddOrder(OrderBefore arg)
+			{
+				arg.SetCustomer(this);
+			}
+
+			public HashSet<OrderBefore> FriendOrders() => _orders;
+
+			private double GetPriceFor(OrderBefore order) => order.GetDiscountedPrice();
+
+			public int GetDiscount() => 0;
+		}
+
+		public class OrderAfter
+		{
+			public CustomerAfter Customer { get; set; }
+
+			public void SetCustomer(CustomerAfter arg)
+			{
+				Customer?.FriendOrders().Remove(this);
+				Customer = arg;
+				Customer?.FriendOrders().Add(this);
+			}
+
+			private int GetGrossPrice() => 0;
+
+			public double GetDiscountedPrice(CustomerAfter customerAfter) => 0.0;
+		}
+
+		public class CustomerAfter
+		{
+			private readonly HashSet<OrderAfter> _orders = new HashSet<OrderAfter>();
+
+			private void AddOrder(OrderAfter arg)
+			{
+				arg.SetCustomer(this);
+			}
+
+			public HashSet<OrderAfter> FriendOrders() => _orders;
+
+			public double GetPriceFor(OrderAfter order) =>
+				_orders.Contains(order) ? order.GetDiscountedPrice(this) : double.NaN;
+
+			public static int GetDiscount() => 0;
+		}
+	}
+}
--- Refactoring/Tips/ChangeUnidirectionalAssociationToBidirectional.cs
+++ Refactoring/Tips/ChangeUnidirectionalAssociationToBidirectional.cs
@@ -4,43 +4,54 @@ namespace Refactoring.Tips
 {
 	public class ChangeUnidirectionalAssociationToBidirectional
 	{
-		public class Order
+		public class OrderBefore
 		{
-			private readonly List<Customer> _customers = new List<Customer>();
-			public Customer Customer { get; private set; }
+			public CustomerBefore Customer { get; set; }
+		}
 
-			public void SetCustomer(Customer arg)
+		public class CustomerBefore
+		{
+			private readonly HashSet<OrderBefore> _orders = new HashSet<OrderBefore>();
+			private HashSet<OrderBefore> FriendOrders() => _orders;
+		}
+
+		public class OrderAfter
+		{
+			private readonly List<CustomerAfter> _customers = new List<CustomerAfter>();
+			public CustomerAfter CustomerAfter { get; private set; }
+
+			public void SetCustomer(CustomerAfter arg)
 			{
-				Customer?.FriendOrders().Remove(this);
-				Customer = arg;
-				Customer?.FriendOrders().Add(this);
+				CustomerAfter?.FriendOrders().Remove(this);
+				CustomerAfter = arg;
+				CustomerAfter?.FriendOrders().Add(this);
 			}
 
-			public void AddCustomer(Customer arg)
+			public void AddCustomer(CustomerAfter arg)
 			{
 				arg.FriendOrders().Add(this);
 				_customers.Add(arg);
 			}
 
-			public void RemoveCustomer(Customer arg)
+			public void RemoveCustomer(CustomerAfter arg)
 			{
 				arg.FriendOrders().Remove(this);
 				_customers.Remove(arg);
 			}
 		}
 
-		public class Customer
+		public class CustomerAfter
 		{
-			private readonly HashSet<Order> _orders = new HashSet<Order>();
+			private readonly HashSet<OrderAfter> _orders = new HashSet<OrderAfter>();
 
-			public HashSet<Order> FriendOrders() => _orders;
+			public HashSet<OrderAfter> FriendOrders() => _orders;
 
-			private void AddOrder(Order arg)
+			private void AddOrder(OrderAfter arg)
 			{
 				arg.SetCustomer(this);
 			}
 
-			private void RemoveOrder(Order arg)
+			private void RemoveOrder(OrderAfter arg)
 			{
 				arg.RemoveCustomer(this);
 			}
