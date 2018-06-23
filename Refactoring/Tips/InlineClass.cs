c0387b477109683773f4caf526edc618c46aca52
Piotr Czech <piterczech@live.com>
2018-06-23 18:10:46 +0200

    commit

--- Refactoring/Tips/InlineClass.cs
+++ Refactoring/Tips/InlineClass.cs
@@ -1,7 +1,68 @@
 ﻿namespace Refactoring.Tips
 {
-    public class InlineClass
-    {
+	public class InlineClassBefore
+	{
+		private class Person
+		{
+			private readonly TelephoneNumber _officeTelephone = new TelephoneNumber();
+			private string _name;
 
-    }
+			public string GetName() => _name;
+
+			public string GetTelephoneNumber() => _officeTelephone.GetTelephoneNumber();
+
+			private TelephoneNumber GetOfficeTelephone() => _officeTelephone;
+		}
+
+		private class TelephoneNumber
+		{
+			private string _areaCode;
+
+			private string _number;
+
+			public string GetTelephoneNumber() => $"( {_areaCode} ) {_number}";
+
+			private string GetAreaCode() => _areaCode;
+
+			private void SetAreaCode(string arg)
+			{
+				_areaCode = arg;
+			}
+
+			private string GetNumber() => _number;
+
+			private void SetNumber(string arg)
+			{
+				_number = arg;
+			}
+		}
+	}
+
+	public class InlineClassAfter
+	{
+		private class Person
+		{
+			private string _areaCode;
+			private string _name;
+			private string _number;
+
+			public string GetName() => _name;
+
+			public string GetTelephoneNumber() => $"( {_areaCode} ) {_number}";
+
+			private string GetAreaCode() => _areaCode;
+
+			private void SetAreaCode(string arg)
+			{
+				_areaCode = arg;
+			}
+
+			private string GetNumber() => _number;
+
+			private void SetNumber(string arg)
+			{
+				_number = arg;
+			}
+		}
+	}
 }
