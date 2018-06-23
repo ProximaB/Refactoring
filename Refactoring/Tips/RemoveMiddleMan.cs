795daad00132fd48bc1f85e07e28c415fb26c4b2
Piotr Czech <piterczech@live.com>
2018-06-23 19:32:06 +0200

    commit

---
+++ Refactoring/Tips/RemoveMiddleMan.cs
@@ -0,0 +1,54 @@
+﻿namespace Refactoring.Tips
+{
+	public class RemoveMiddleMan
+	{
+		public class HideDelegateBefore
+		{
+			private class Person
+			{
+				private Department _department;
+
+				public Department GetDepartment() => _department;
+
+				public void SetDepartment(Department arg)
+				{
+					_department = arg;
+				}
+
+				public Person GetManager() => _department.GetManager();
+			}
+
+			private class Department
+			{
+				private readonly Person _manager;
+				private string _chargeCode;
+
+				public Department(Person manager) => _manager = manager;
+
+				public Person GetManager() => _manager;
+			}
+		}
+
+		private class Person
+		{
+			private Department _department;
+
+			public Department GetDepartment() => _department;
+
+			public void SetDepartment(Department arg)
+			{
+				_department = arg;
+			}
+		}
+
+		private class Department
+		{
+			private readonly Person _manager;
+			private string _chargeCode;
+
+			public Department(Person manager) => _manager = manager;
+
+			public Person GetManager() => _manager;
+		}
+	}
+}
