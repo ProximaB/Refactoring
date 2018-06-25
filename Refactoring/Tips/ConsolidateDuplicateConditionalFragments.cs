b0b8f47601ead48a674a0e5daf4d270b5875c613
Piotr Czech <piterczech@live.com>
2018-06-25 18:23:02 +0200

    commit

---
+++ Refactoring/Tips/ConsolidateDuplicateConditionalFragments.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Refactoring.Tips
+{
+	public class ConsolidateDuplicateConditionalFragments
+	{
+		private double _price;
+		private double _total;
+
+		public void Before()
+		{
+			if (IsSpecialDeal())
+			{
+				_total = _price * 0.95;
+				Send();
+			}
+			else
+			{
+				_total = _price * 0.98;
+				Send();
+			}
+		}
+
+		private static void Send()
+		{
+			Console.WriteLine("Hello!");
+		}
+
+		private static bool IsSpecialDeal() => true;
+
+		public void After()
+		{
+			if (IsSpecialDeal())
+				_total = _price * 0.95;
+			else
+				_total = _price * 0.98;
+			Send();
+		}
+	}
+}
---
+++ Refactoring/Tips/ReplaceSubclassWithFields.cs
@@ -0,0 +1,42 @@
+﻿namespace Refactoring.Tips
+{
+    public class ReplaceSubclassWithFields
+    {
+	    public abstract class PersonBefore
+	    {
+			public abstract bool IsMale();
+		    public abstract char GetCode();
+		}
+
+	    public class Male : PersonBefore
+	    {
+		    public override bool IsMale() => true;
+
+		    public override char GetCode() => 'M';
+	    }
+
+	    public class Female : PersonBefore
+	    {
+		    public override bool IsMale() => false;
+
+		    public override char GetCode() => 'F';
+	    }
+
+	    public class PersonAfter
+	    {
+		    private readonly bool _isMale;
+		    private char Code { get; }
+
+		    public PersonAfter(bool isMale, char code)
+		    {
+			    _isMale = isMale;
+			    Code = code;
+		    }
+
+		    public bool IsMale()
+		    {
+			    return _isMale;
+		    }
+	    }
+    }
+}
