5cb021227e71a5f3db62b2d023ec2e9a4be7df9d
Piotr Czech <piterczech@live.com>
2018-06-27 10:07:24 +0200

    commit

---
+++ Refactoring/Tips/RenameMethod.cs
@@ -0,0 +1,14 @@
+﻿namespace Refactoring.Tips
+{
+	public class RenameMethod
+	{
+		private int _officeAreaCode;
+		private int _officeNumber;
+
+		public string GetTelephoneNumberBefore() => $"( {_officeAreaCode.ToString()} ) {_officeNumber.ToString()}";
+
+		public string GetTelephoneNumberAfter() => GetOfficeTelephoneNumber();
+
+		private string GetOfficeTelephoneNumber() => $"( {_officeAreaCode.ToString()} ) {_officeNumber.ToString()}";
+	}
+}
---
+++ Refactoring/Tips/SeparateQueryFromModifier.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Refactoring.Tips
+{
+	public class SeparateQueryFromModifier
+	{
+		private string FoundMiscreantBefore(string[] people)
+		{
+			for (var i = 0; i < people.Length; i++)
+			{
+				if (people[i].Equals("Don"))
+				{
+					SendAlert();
+					return "Don";
+				}
+
+				if (people[i].Equals("John"))
+				{
+					SendAlert();
+					return "John";
+				}
+			}
+
+			return string.Empty;
+		}
+
+		private void CheckSecurityBefore(string[] people)
+		{
+			SomeLaterCode(FoundMiscreantBefore(people));
+		}
+
+		private static void SendAlert()
+		{
+			Console.WriteLine("Hello!");
+		}
+
+		private static void SomeLaterCode(string found)
+		{
+			Console.WriteLine(found);
+		}
+
+
+		private static void SendAlert(string[] people)
+		{
+			if (!FoundPerson(people).Equals("")) SendAlert();
+		}
+
+		private static string FoundPerson(string[] people)
+		{
+			for (var i = 0; i < people.Length; i++)
+			{
+				if (people[i].Equals("Don")) return "Don";
+				if (people[i].Equals("John")) return "John";
+			}
+
+			return string.Empty;
+		}
+
+		private void CheckSecurity(string[] people)
+		{
+			SendAlert(people);
+			SomeLaterCode(FoundPerson(people));
+		}
+	}
+}
